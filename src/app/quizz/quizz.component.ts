7bc878aa7d041da1ee7029a14fe5c38e5251c2c5
Oana Blagoe <oana.blagoe@gmail.com>
2018-11-25 03:11:34 +0200

    argh

--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -2,6 +2,7 @@ import {Component, Injectable, OnInit} from '@angular/core';
 import {QuizModel} from '../../models/quiz-model';
 import {AnswerModel} from '../../models/answer-model';
 import {QuestionModel} from '../../models/question-model';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-quizz',
@@ -12,23 +13,38 @@ import {QuestionModel} from '../../models/question-model';
 @Injectable()
 export class QuizzComponent implements OnInit {
   public quizModel: QuizModel;
-
+  quizForm: FormGroup;
+  ans: AnswerModel;
+  ansarr: AnswerModel[] = [];
+  qmarr: QuestionModel[] = [];
   constructor() {
   }
 
   ngOnInit() {
-    this.quizModel = new QuizModel('huh???');
-    const ans = new AnswerModel ('yasss', true);
-    const ansarr: AnswerModel[] = [ans];
-    // ansarr.push(ans);
-    const qm = new QuestionModel ( 'yas or pass???');
-    qm._answers = ansarr;
-    const qmarr: QuestionModel[] = [qm];
-    // qmarr.push(qm);
-    this.quizModel._questions = qmarr;
+    for (let j = 0; j < 4; j++) {
+      const qm = new QuestionModel(this.makeRandom());
+      for (let i = 0; i < 4; i++) {
+        this.ans = new AnswerModel(this.makeRandom(), true);
+        this.ansarr.push(this.ans);
+      }
+      qm._answers = this.ansarr;
+      this.qmarr.push(qm);
+    }
+    this.quizModel._questions = this.qmarr;
+  }
+
+  onSubmit() {
     console.log(this.quizModel.title);
   }
-   onSave() {
-    console.log(this.quizModel.title);
-   }
+
+  makeRandom() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
 }
